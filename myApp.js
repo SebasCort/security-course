806b7b06f30e720f87f5431e450db3e932231b39
SebasCort <166546838+SebasCort@users.noreply.github.com>
2024-04-10 21:18:54 +0000

    helmet.dnsPrefetchControl

--- myApp.js
+++ myApp.js
@@ -12,6 +12,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny",}));
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds , force:true}));
 app.use(helmet.xssFilter());
+app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
