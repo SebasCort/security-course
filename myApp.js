db019d498cb3ab9130fc80b51367e5c4909470be
SebasCort <166546838+SebasCort@users.noreply.github.com>
2024-04-10 21:11:21 +0000

    helmet.hsts testing

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-
+const ninetyDaysInSeconds = 90*24*60*60;
 const PORT = process.env.PORT || 3000;
 
 
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny",}));
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds , force:true}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
