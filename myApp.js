8550305c5bde7738b28e745e0c0d1b28f36aaf7a
SebasCort <166546838+SebasCort@users.noreply.github.com>
2024-04-10 18:26:27 +0000

    update code

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 
 
@@ -57,7 +57,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
